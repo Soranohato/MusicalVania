5f1d37da77c68863e4732900502d983c0f7e8bed
Pyrousha <47787636+Pyrousha@users.noreply.github.com>
2023-11-15 01:04:27 -0600

    Basic player movement

---
+++ Scripts/PlayerController.gd
@@ -0,0 +1,54 @@
+extends CharacterBody2D
+
+var maxSpeed = 600;
+var accelSpeed = 6000;
+var frictionSpeed = 3000;
+
+var gravUp = 1750;
+var gravDown = 3000;
+
+var jumpStr = 1000;
+var grounded = false;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if (event.is_action_pressed("Jump") && grounded):
+		velocity.y = -jumpStr;
+	else:
+		if (event.is_action_released("Jump") && !grounded && velocity.y < 0):
+			velocity.y *= 0.6;
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#Get Input
+	var xInput = 0;
+	if Input.is_action_pressed("Right"):
+		xInput += 1;
+	if Input.is_action_pressed("Left"):
+		xInput -= 1;
+	
+	if xInput == 0: #todo: change this so it works for controllers
+		#Apply Friction
+		if velocity.x >= 0:
+			velocity.x = maxf(0, velocity.x - frictionSpeed * delta);
+		else:
+			velocity.x = minf(0, velocity.x + frictionSpeed * delta);
+		
+	
+	velocity.x = clampf(velocity.x + xInput * accelSpeed * delta, -maxSpeed, maxSpeed);
+#	print(velocity.x);
+
+	if(!grounded):
+		#apply gravity
+		if(velocity.y < 0 && Input.is_action_pressed("Jump")):
+			#going up and holding jump, so use weaker gravity
+			velocity.y += gravUp * delta;
+		else:
+			velocity.y += gravDown * delta;
+	
+	move_and_slide();
+	grounded = is_on_floor();
+	pass;
