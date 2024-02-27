6aa680e4ad61889d905ce9e3aa515ec0c8e8365c
Flame48 <advait.s.rajan@gmail.com>
2024-02-27 16:03:01 -0600

    Character now faced direction of motion.

--- Scripts/Component Scripts/MovementComponent.gd
+++ Scripts/Component Scripts/MovementComponent.gd
@@ -13,6 +13,8 @@ var horizontalFriction: float = 200
 @export
 var acceleration: float
 
+var facingDirection: bool = false
+
 func applyFriction(delta: float):
 		if parent.velocity.x >= 0:
 			parent.velocity.x = maxf(0, parent.velocity.x - horizontalFriction * delta);
@@ -20,8 +22,21 @@ func applyFriction(delta: float):
 			parent.velocity.x = minf(0, parent.velocity.x + horizontalFriction * delta);
 
 func accelerate(direction:float, delta: float):
+	
 	if (direction==0):
 		applyFriction(delta)
 	else:
 		parent.velocity.x = clampf(parent.velocity.x + direction * acceleration * delta, -MAX_VELOCITY, MAX_VELOCITY);
+	
+	var newDirection: bool = false
+	
+	if (parent.velocity.x < 0):
+		newDirection = true
+	elif (parent.velocity.x > 0):
+		newDirection = false
+		
+	if (parent.velocity.x != 0) and (facingDirection!=newDirection):
+		parent.scale.x = -1
+		facingDirection = newDirection
+		
 
