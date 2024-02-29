bd22dfe59079278db4d857dca24f89a9c394e9e3
Flame48 <advait.s.rajan@gmail.com>
2024-02-29 10:17:41 -0600

    Added a sweeping hitbox comp

---
+++ Scenes/Components/SweepingHitboxComponent.gd
@@ -0,0 +1,12 @@
+extends HitboxComponent
+
+class_name SweepingHitboxComponent
+
+@export
+var sweepAnimation: AnimationNode
+
+func startAttack():
+	super.startAttack()
+
+func stopAttack():
+	super.stopAttack()
